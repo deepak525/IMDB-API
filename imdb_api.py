ac80e2da236b473520d3d9d9146eecdde34439d9
Shivam Chand Deopa <shivamdeopa@gmail.com>
2019-10-18 13:20:41 +0530

    Added the request methods
    Addded request methods for each route decorator and removed redundant calling of pyMongo since, by default it chooses 27017 port.

--- imdb_api.py
+++ imdb_api.py
@@ -10,10 +10,9 @@ from pymongo import MongoClient
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def index():
     client = MongoClient() 
-    client = MongoClient('mongodb://localhost:27017/')
     db = client['IMDB']
     collection = db['movies_data']
     d=[]
@@ -24,10 +23,9 @@ def index():
     mydata["list"]=d
     return render_template("index.html",data=mydata)
 
-@app.route("/movie1/<ids>")
+@app.route("/movie1/<ids>", methods=["GET"])
 def movie1(ids):
     client = MongoClient() 
-    client = MongoClient('mongodb://localhost:27017/')
     db = client['IMDB']
     collection = db['movies_data']
     curr = collection.find({'id':ids})
@@ -47,7 +45,6 @@ def movie_name():
     if(request.method=="POST"):
         name = request.form['movie_title']
         client = MongoClient() 
-        client = MongoClient('mongodb://localhost:27017/')
         db = client['IMDB']
         collection = db['movies_data']
         curr = collection.find({'movie_name':name})
@@ -67,10 +64,9 @@ def my_from():
     text =  request.form['movieID']
     return redirect(url_for('movie1',ids=text))
 
-@app.route("/api2")
+@app.route("/api2", methods=["GET"])
 def api2():
     client = MongoClient() 
-    client = MongoClient('mongodb://localhost:27017/')
     db = client['IMDB']
     collection = db['movies_data']
     d=[]
